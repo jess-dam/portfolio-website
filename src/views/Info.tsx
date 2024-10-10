0ef290ea791fafdbed3a8741ebc885b008098793
Jess Dam <jess@greenworkx.org>
2024-10-10 10:16:46 +0100

    add breakpoints for skills

--- src/views/Info.tsx
+++ src/views/Info.tsx
@@ -4,7 +4,7 @@ function Info() {
   return (
     <SectionContainer bgColor="white" textColor="black">
       <div className="grid grid-rows-auto">
-        <div className="top-0 border border-black w-screen h-[50px]"></div>
+        <div className="top-0 border-b border-black w-screen h-[50px]"></div>
 
         <div className="flex justify-center items-center flex-wrap md:flex-nowrap flex-col md:flex-row align-middle space-x-4 gap-8 py-10">
           <div className="bg-black rounded-full w-[125px] h-[250px] md:w-[250px] md:h-[500px] justify-center">
@@ -12,7 +12,7 @@ function Info() {
           </div>
           <div className="col-span-2">
             {/* <h2 className='pb-10 text-secondary text-center md:text-left'>Info</h2> */}
-            <p className="pb-5 px-4 font-bold uppercase text-2xl pr-4 max-w-[600px]">
+            <p className="pb-5 px-4 font-bold uppercase text-2xl pr-4 max-w-[600px] text-black">
               <span className="text-secondary">
                 I’m a full stack developer with skills in UX.
               </span>{' '}
