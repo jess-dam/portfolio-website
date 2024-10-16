b219b40e6e5b61c1de093fed01a310557e8dea3e
Jess Dam <jess@greenworkx.org>
2024-10-16 09:44:17 +0100

    Remove unused import

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,3 @@
-import SectionContainer from "./SectionContainer";
-
 function Footer() {
   return <div className='w-full bg-white text-black'>
     <div className="grid grid-cols-2 md:grid-cols-4 grid-rows-2 md:grid-rows-4 h-[300px] p-4 pb-6 md:p-10 gap-4 md:gap-10  uppercase">
