575a1dde9ce4cc4f48b3a7c9f558929a2d3c7395
Jess Dam <jess@greenworkx.org>
2024-10-21 12:18:33 +0100

    Adjust layout for skills content

--- src/views/Skills.tsx
+++ src/views/Skills.tsx
@@ -3,9 +3,9 @@ import { SECTION_REFS } from '../pageRefs';
 
 const SkillsCollection = ({ skills }: { skills: string[] }) => {
   return (
-    <div className='flex flex-wrap gap-2 p-4 px-4 md:px-10'>
+    <div className='flex flex-wrap gap-2'>
       { skills.map(skill => 
-        <label className='bg-primary text-white shadow-md place-content-center uppercase font-semibold text-sm md:text-md tracking-[4px] p-2 px-4 rounded-full'>
+        <label className='h-fit bg-primary text-white shadow-md place-content-center uppercase font-semibold text-sm md:text-md tracking-[4px] p-2 px-4 rounded-full'>
           {skill}
         </label>
       )}
@@ -20,23 +20,23 @@ function Skills() {
 
   return (
     <SectionContainer id={SECTION_REFS.SKILLS} bgColor="black" textColor="black">
-      <div className="flex justify-center items-center bg-black p-2 pb-10 max-w-[1500px] md:min-w-[60vw]">
-        <div className="grid grid-cols-6 grid-rows-5 py-5 md:px-5 gap-2 max-w-[95vw] h-auto">
-          <div className="grid col-span-6 md:col-span-3 pb-10 row-span-6 bg-accent rounded-xl border-black">
-            <h2 className="p-4 md:p-6">Strengths</h2>
+      <div className="flex items-center bg-black p-2 py-40 max-w-[1500px] md:min-w-[60vw] ">
+        <div className="grid auto-rows-min md:grid-cols-6 md:grid-rows-4 gap-2 h-fit">
+          <div className="grid row-start-1 col-span-6 md:row-span-3 md:col-span-3 xl:col-span-4  bg-accent rounded-xl border-black p-6 gap-4">
+            <h2>Strengths</h2>
             <SkillsCollection skills={strengths} />
             
             {/* Divider */}
             <hr className='flex justify-self-center mt-10 border-t border-gray-500 w-[80%]' />
 
-            <h2 className='p-4 md:p-6'>Soft Skills</h2>
+            <h2>Soft Skills</h2>
             <SkillsCollection skills={softSkills} />
 
           </div>
-          <div className="col-span-6 md:col-span-3 row-span-3 bg-white rounded-xl border-black">
-            <h2 className="p-4 md:p-6">Interested In</h2>
-            <div className='grid grid-cols-4 justify-items-centerr gap-8 p-4 md:p-4'>
-              <div className='flex flex-col items-center gap-4'>
+          <div className="h-fit row-start-3 col-span-6 md:col-span-3 xl:col-span-2 md:row-span-3 bg-white rounded-xl border-black text-primary p-6">
+            <h2>Interested In</h2>
+            <div className='grid grid-cols-2 grid-rows-2 h-full place-items-center py-8 gap-8 p-4 md:p-8'>
+              <div className='flex flex-col items-center gap-4 text-xs md:text-sm'>
                 <img src='/assets/images/doodles/ux.svg' alt='' width={100}/>
                 <label className='uppercase text-sm tracking-widest text-center'>UX</label>
               </div>
@@ -54,8 +54,8 @@ function Skills() {
               </div>
             </div>
           </div>
-          <div className="col-span-6 md:col-span-3 row-span-3 bg-white rounded-xl border-black">
-            <h2 className="bottom-0 right-0 p-4 md:p-6">Currently Growing In</h2>
+          <div className="h-fit row-start-2 col-span-6 md:row-span-1 bg-white rounded-xl border-black p-6 gap-4">
+            <h2 className='mb-8'>Currently Growing In</h2>
             <SkillsCollection skills={growingSkills} />
           </div>
         </div>
