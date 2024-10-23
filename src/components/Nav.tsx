60dd0fc3ba648dc4c1c605c1b7b5f1dcd6a8b4da
Jess Dam <jess@greenworkx.org>
2024-10-23 16:26:07 +0100

    hide nav bar on on down scroll

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,15 +1,39 @@
 import { useState } from 'react';
 import { SECTION_REFS } from '../pageRefs';
+import { easeInOut, motion, useMotionValueEvent, useScroll } from 'framer-motion';
 
 function Nav() {
-  const [isMobileDropdownHidden, setIsMobileDropdownHidden] =
+  const [isUsingMobileDropdown, setIsUsingMobileDropdown] =
     useState<boolean>(true);
+
+    const [isNavHidden, setIsNavHidden] = useState<boolean>(false);
 
   const linkedinUrl = 'https://www.linkedin.com/in/jess-dam-507485165/';
   const githubUrl = 'https://github.com/jess-dam';
 
+  const { scrollY } = useScroll();
+
+  useMotionValueEvent(scrollY, 'change', (latest) => {
+    // Compare the previous y coordinates to the current ones to check if
+    // the user is scrolling up the page
+    const previous = scrollY.getPrevious();
+    if (previous && previous < latest) {
+        setIsNavHidden(true);
+    } else {
+        setIsNavHidden(false);
+    }
+  })
+
   return (
-    <div className="fixed w-full z-30 flex items-center text-white">
+    <motion.div
+      variants={{
+        visible: { y: 0 },
+        hidden: { y: '-100%' },
+      }}
+      animate={isNavHidden ? 'hidden' : 'visible'}
+      transition={{ duration: 0.35, easeInOut }}
+      className="fixed w-full z-30 flex items-center text-white"
+    >
       {/* Mobile Nav */}
       <div className="md:hidden flex flex-col w-full px-4 pt-4">
         <div className="w-full flex content-center justify-between gap-8">
@@ -22,14 +46,14 @@ function Nav() {
             <button
               aria-label="Click for drop down menu"
               className="bg-black"
-              onClick={() => setIsMobileDropdownHidden(!isMobileDropdownHidden)}
+              onClick={() => setIsUsingMobileDropdown(!isUsingMobileDropdown)}
             >
               <img src="/assets/images/hamburger_menu.svg" alt="" width={25} />
             </button>
           </div>
         </div>
         <div
-          className={`${isMobileDropdownHidden ? 'hidden' : 'flex'} flex-col place-self-end w-[50vw] text-right mt-4 gap-4 text-bold uppercase tracking-widest text-sm bg-black p-4 rounded-xl`}
+          className={`${isUsingMobileDropdown ? 'hidden' : 'flex'} flex-col place-self-end w-[50vw] text-right mt-4 gap-4 text-bold uppercase tracking-widest text-sm bg-black p-4 rounded-xl`}
         >
           <a href={'#' + SECTION_REFS.INFO}>Info</a>
           <a href={'#' + SECTION_REFS.EXPERIENCE}>Experience</a>
@@ -72,7 +96,7 @@ function Nav() {
           </a>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 }
 
