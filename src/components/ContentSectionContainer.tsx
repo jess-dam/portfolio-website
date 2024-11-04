696ce8270d1f714c4db93c877dc05371942b5260
Jess Dam <jess@greenworkx.org>
2024-11-04 14:15:31 +0000

    Remove rounded border on sections

--- src/components/ContentSectionContainer.tsx
+++ src/components/ContentSectionContainer.tsx
@@ -10,17 +10,10 @@ interface Props {
   id?: string;
   bgColor?: string;
   textColor?: string;
-  roundedBorder?: boolean;
   children: JSX.Element | JSX.Element[];
 }
 
-function ContentSectionContainer({
-  id,
-  children,
-  bgColor,
-  textColor,
-  roundedBorder,
-}: Props) {
+function ContentSectionContainer({ id, children, bgColor, textColor }: Props) {
   const containerRef = useRef(null);
   const isInView = useInView(containerRef, { once: false });
   const sectionFadeInAnimation = useAnimation();
@@ -33,13 +26,13 @@ function ContentSectionContainer({
     <AnimatePresence>
       <motion.section
         id={id}
-        className={`pb-10 flex justify-center place-items-between min-h-screen w-full px-4 overflow-hidden bg-${bgColor} text-${textColor} ${roundedBorder ? 'rounded-t-xl z-10' : ''}`}
+        className={`pb-10 flex justify-center place-items-between min-h-screen w-full px-4 overflow-hidden bg-${bgColor} text-${textColor}`}
         initial={{ opacity: 0.8 }}
         whileInView={{ opacity: 1 }}
         transition={{ ease: 'easeIn', duration: 0.8 }}
       >
         <div
-          className={`pb-10 flex justify-center place-items-between min-h-screen w-full px-4 overflow-hidden bg-${bgColor} text-${textColor} ${roundedBorder ? 'rounded-t-xl z-10' : ''}`}
+          className={`pb-10 flex justify-center place-items-between min-h-screen w-full px-4 overflow-hidden bg-${bgColor} text-${textColor}`}
         >
           {children}
         </div>
